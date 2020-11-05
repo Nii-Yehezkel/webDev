fe67a2c4dd5ef927186d276e4a2495c3bcc1a2c4
Nii Yehezkel <larfelix360@yahoo.com>
2020-11-05 11:14:38 +0000

    modify

--- mainApp/static/javaScript/script.js
+++ mainApp/static/javaScript/script.js
@@ -1,8 +1,32 @@
- document.getElementById("btn").addEventListener("click", () => {
-     document.querySelector(".wrapper").style.display = "block";
-     document.querySelector("#btn").style.display = "none";
- })
- document.querySelector(".close").addEventListener("click", () => {
-     document.querySelector(".wrapper").style.display = "none";
-     document.querySelector("#btn").style.display = "block";
- })
+document.getElementById("btn").addEventListener("click", () => {
+    document.querySelector(".wrapper").style.display = "block";
+    document.querySelector("#btn").style.display = "none";
+})
+
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".wrapper").style.display = "none";
+    document.querySelector("#btn").style.display = "block";
+})
+
+document.querySelector(".pay").addEventListener("click", () => {
+    //document.querySelector(".loanResponse").style.display = "none";
+    document.querySelector(".loanResponse").style.display = "block";
+})
+
+
+
+//import loanProfile_view from
+/*
+
+function handleClick(e) {
+    e.preventDefault()
+    if (onclick(e)=== true){
+        {{response}}
+    }else
+    {
+        window.location.reload(true)
+    }
+
+}
+*/
+
--- mainApp/views.py
+++ mainApp/views.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 from requests.auth import HTTPBasicAuth
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+#from import_export export loanProfile_view
 
 import requests
 import json
+import pprint
 
 
 # Create views here.
@@ -13,22 +16,26 @@ def home_view(request):
         url = 'https://films.dalexapps.com/FilmsWeb/api/web/loginuser'
         username = request.POST.get('Username')
         password = request.POST.get('Password')
-        headers = {'Content-Type': 'application/json'}
-        LogUser = requests.post(url+'?username='+username+'&'+'password='+password, headers = headers)
-        print(LogUser.content)
-        return redirect('loanprofile')
+        if username == '56-0001-00018' and password == '9999':
+            headers = {'Content-Type': 'application/json'}
+            LogUser = requests.post(url+'?username='+username+'&'+'password='+password, headers = headers)
+            return redirect('loanprofile')
+        else:
+            print('Not correct')
     return render(request, 'mainApp/weblinq.html')
 
-def getId():
-    url = 'https://films.dalexapps.com/FilmsWeb/api/web/getloanrequest'
-    id = request.GET.get('LoanID')
-    #id = '6'
-    headers = {'Content-Type': 'application/json'}
-    LoanProfile = requests.get(url+'?id='+id, headers = headers)
-    print(LoanProfile.content)
+
+
 
 
 
 def loanProfile_view(request):
-
-    return render(request, 'mainApp/loanProfile.html', getId())
+    body = 'No response yet, Enter Loan ID'
+    url = 'https://films.dalexapps.com/FilmsWeb/api/web/getloanrequest'
+    id = request.POST.get('LoanID')
+    if id is not None:
+        headers = {'Content-Type': 'application/json'}
+        LoanProfile = requests.get(url+'?id='+id, headers = headers)
+        body = json.loads(LoanProfile.content)
+        pprint.pprint(body)
+    return render(request, 'mainApp/loanProfile.html', {'response': body})
